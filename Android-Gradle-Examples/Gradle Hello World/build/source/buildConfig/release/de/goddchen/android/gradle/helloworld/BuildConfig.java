a1ec42e726ebcbc2fb280345abe7c3a714cf93b6
kyk555 <195827995@qq.com>
2014-10-07 19:38:46 +0800

    clean ..

---
+++ Android-Gradle-Examples/Gradle Hello World/build/source/buildConfig/release/de/goddchen/android/gradle/helloworld/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.goddchen.android.gradle.helloworld;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "de.goddchen.android.gradle.helloworld";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
